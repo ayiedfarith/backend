4858d02061d857682885ef6d603db439e6827906
Ayied Farith <syariffudin97farith@yahoo.com>
2020-10-23 19:50:52 +0800

    Todo vue.js x

---
+++ models/todo.js
@@ -0,0 +1,13 @@
+  
+module.exports = (sequelize, DataTypes) => {
+	const Todos = sequelize.define('Todos', {
+        title: DataTypes.STRING,
+		description: DataTypes.STRING
+	});
+
+	Todos.associate = function(models) {
+		Todos.hasMany(models.Comments)
+	}
+
+	return Todos;
+};
